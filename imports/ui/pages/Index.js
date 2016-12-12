41ddffa1b7071d415317d7f119028644882bcea7
Anton Savchenko <savchenko.inc@gmail.com>
2016-12-12 20:19:42 +0200

    fix highlighting text

--- imports/ui/pages/Index.js
+++ imports/ui/pages/Index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactQuill from 'react-quill';
+import {debounce} from 'throttle-debounce';
 
 class Index extends React.Component {
 	constructor(props) {
@@ -7,11 +8,22 @@ class Index extends React.Component {
 		this.handleChange = this.handleChange.bind(this);
 		this.createDoc = this.createDoc.bind(this);
 		this.state = { text: 'Fusce dapibus, tellus ac cursus commodo' };
+		this.saveText = debounce(900, this.saveText);
 	}
+	saveText(value){
+	    console.log('saveText ran');
+	    Meteor.call('updateText', value, this.props.thisDocument._id, function(error, response){
+	      if (error) { console.log(error); return; }
+	    });
+	 }
 	handleChange(text) {
-    	Meteor.call('updateText', text, this.props.thisDocument._id)
+		console.log(text);
+		this.saveText(text);
   	}
+	componentWillReceiveProps() {
+		this.refs.editor.blur();
 
+	}
 	createDoc() {
     	Meteor.call('createDoc');
   	}
@@ -22,8 +34,9 @@ class Index extends React.Component {
 		return (<div className="Index">
 		{!thisDocument && <button onClick={this.createDoc}>create a doc</button>}
 		
-			        <h3>Editor #2</h3>
+			        <h3>Editor #2</h3>	
 				    <ReactQuill
+				    	ref="editor"
 				    	value={thisDocument && thisDocument.body ? thisDocument.body : ''}
 						onChange={this.handleChange}
 				    />
